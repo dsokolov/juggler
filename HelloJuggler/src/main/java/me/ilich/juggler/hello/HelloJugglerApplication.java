3e40e7db58b65092af945141b59464ab9839b946
dmitry.i.sokolov <DmitryIlichSokolov@gmail.com>
2015-12-24 16:04:19 +0400

    init

---
+++ HelloJuggler/src/main/java/me/ilich/juggler/hello/HelloJugglerApplication.java
@@ -0,0 +1,12 @@
+package me.ilich.juggler.hello;
+
+import android.app.Application;
+
+public class HelloJugglerApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+    
+}
---
+++ juggler/src/main/java/me/ilich/juggler/Screen.java
@@ -0,0 +1,5 @@
+package me.ilich.juggler;
+
+public abstract class Screen {
+
+}
