74c42c7e36101664269891f2be6296dbbf255b29
Dmitry I. Sokolov <DmitryIlichSokolov@gmail.com>
2015-12-27 08:16:10 +0400

    вызов метода по аннотации

--- juggler/src/main/java/me/ilich/juggler/Screen.java
+++ juggler/src/main/java/me/ilich/juggler/Screen.java
@@ -53,14 +53,25 @@ public abstract class Screen<P extends Screen.Params> {
         return new ContentFragmentFactory() {
             @Override
             public JugglerContentFragment create(Params params) {
-                JugglerContentFragment r = null;
+                JugglerContentFragment oneParamsFragment = null;
+                JugglerContentFragment noParamsFragment = null;
                 if (screenClass.isAnnotationPresent(JugglerContent.class)) {
                     JugglerContent content = screenClass.getAnnotation(JugglerContent.class);
                     Class<? extends JugglerContentFragment> clazz = content.aClass();
                     try {
                         for (Method method : clazz.getMethods()) {
                             if (method.isAnnotationPresent(JugglerNewInstance.class)) {
-                                r = (JugglerContentFragment) method.invoke(null, params);
+                                Class[] methodParams = method.getParameterTypes();
+                                switch (methodParams.length) {
+                                    case 1:
+                                        if (params.getClass().equals(methodParams[0])) {
+                                            oneParamsFragment = (JugglerContentFragment) method.invoke(null, params);
+                                        }
+                                        break;
+                                    case 0:
+                                        noParamsFragment = (JugglerContentFragment) method.invoke(null, null);
+                                        break;
+                                }
                             }
                         }
                     } catch (InvocationTargetException e) {
@@ -68,6 +79,12 @@ public abstract class Screen<P extends Screen.Params> {
                     } catch (IllegalAccessException e) {
                         e.printStackTrace();
                     }
+                }
+                JugglerContentFragment r = null;
+                if (oneParamsFragment != null) {
+                    r = oneParamsFragment;
+                } else if (noParamsFragment != null) {
+                    r = noParamsFragment;
                 }
                 return r;
             }
